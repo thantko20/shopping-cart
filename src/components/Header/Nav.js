75dae15cdabd9f6ee5de71159946a631e6189d08
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-19 18:59:35 +0630

    Use more semantic html tags

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -1,6 +1,7 @@
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import StyledLink from '../generic/StyledLink';
 
@@ -13,13 +14,13 @@ const Nav = () => {
   return (
     <StyledNav>
       <li>
-        <StyledLink to='/shop'>Shop</StyledLink>
+        <StyledLink to='shop'>Shop</StyledLink>
       </li>
       <li>
-        <StyledLink to='/about'>About</StyledLink>
+        <StyledLink to='about'>About</StyledLink>
       </li>
-      <li aria-label='Cart Icon'>
-        <StyledLink to='/cart'>
+      <li aria-label='Cart'>
+        <StyledLink to='cart'>
           <FontAwesomeIcon icon={faCartShopping} />
         </StyledLink>
       </li>
