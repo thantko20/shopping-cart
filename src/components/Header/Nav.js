f28e79eb740e25b4a1b999998daab5e066278e1c
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-19 15:08:37 +0630

    Style

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -1,23 +1,29 @@
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import styled from 'styled-components';
 import StyledLink from '../generic/StyledLink';
+
+const StyledNav = styled.ul`
+  display: flex;
+  gap: 2rem;
+`;
 
 const Nav = () => {
   return (
-    <ul>
+    <StyledNav>
       <li>
         <StyledLink to='/shop'>Shop</StyledLink>
       </li>
       <li>
         <StyledLink to='/about'>About</StyledLink>
       </li>
-      <li>
+      <li aria-label='Cart Icon'>
         <StyledLink to='/cart'>
           <FontAwesomeIcon icon={faCartShopping} />
         </StyledLink>
       </li>
-    </ul>
+    </StyledNav>
   );
 };
 
