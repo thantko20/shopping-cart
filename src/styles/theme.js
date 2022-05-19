0d21e31a0951b0afba6fed0bbfcee0f7d1ea025e
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-19 13:09:34 +0630

    Add theme

---
+++ src/styles/theme.js
@@ -0,0 +1,26 @@
+const theme = {
+  color: {
+    mainBlue: '#3069db',
+    lightBlue: '#6091f2',
+    lightestBlue: '#b2ccff',
+    darkBlue: '#1e4799',
+    darkestBlue: '#011235',
+    gray: '#a5a5a5',
+    lightGray: 'e8e8e8',
+    darkGray: '161616',
+  },
+  fontSize: {
+    textSmall: '.8rem',
+    textNormal: '1rem',
+    textHeading1: '2rem',
+    textHeading2: '1.5rem',
+    textHeading3: '1.2rem',
+  },
+  bold: {
+    headingBold1: '800',
+    headingBold2: '600',
+    normalBold: '500',
+  },
+};
+
+export default theme;
