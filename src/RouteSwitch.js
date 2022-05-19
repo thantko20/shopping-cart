81a3bc60a539210cc2b571237b4927de75704eca
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-19 17:42:45 +0630

    Refactor routing

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
 
 const RouteSwitch = ({ cart, addToCart, deleteFromCart }) => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='/shop' element={<Shop addToCart={addToCart} />} />
-          <Route path='/about' element={<About />} />
-          <Route
-            path='/cart'
-            element={<Cart cart={cart} deleteFromCart={deleteFromCart} />}
-          />
-        </Route>
-      </Routes>
+      <App />
     </BrowserRouter>
   );
 };
