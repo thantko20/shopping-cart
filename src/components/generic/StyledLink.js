eb986ae8627ff3a75cf351bd7ba78d6f0598d284
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-19 14:24:36 +0630

    Add modified Link from react-router

---
+++ src/components/generic/StyledLink.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+
+  &:active {
+    color: none;
+  }
+`;
