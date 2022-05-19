0ead5db5a4cd79d752b9cc808f7ba01d6ee8be40
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-19 18:49:55 +0630

    Fix routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React, { useState } from 'react';
-import styled, { ThemeProvider, Outlet } from 'styled-components';
-import theme from './styles/theme';
-import Header from './components/Header/Header';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Shop from './components/Shop/Shop';
 import Cart from './components/Cart/Cart';
-
-const Layout = styled.div`
-  min-height: 100vh;
-`;
+import Layout from './components/Layout';
 
 const App = () => {
   const [cart, setCart] = useState([]);
@@ -26,15 +20,16 @@ const App = () => {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Header />
+    <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/shop' element={<Shop />} />
-        <Route path='/cart' element={<Cart />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='shop' element={<Shop />} />
+          <Route path='cart' element={<Cart />} />
+        </Route>
       </Routes>
-    </ThemeProvider>
+    </BrowserRouter>
   );
 };
 
