13220a705cbdd809c2ba77ccc1293251878a97d8
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-20 18:25:48 +0630

    Make header sticky element

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import Nav from './Nav';
 
 const Header = () => {
   return (
-    <header className='flex justify-between px-10 py-3'>
+    <header className='flex flex-col justify-between items-center px-10 py-3 lg:flex-row gap-4 mb-4 shadow-md sticky top-0 bg-inherit'>
       <h1 className='text-4xl font-bold font-lobster underline transition-colors duration-300 hover:text-blue-500'>
         <Link to='/'>goodies</Link>
       </h1>
