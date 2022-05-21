2c881580a5664083281612984afeeb11107f846a
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-21 20:47:12 +0630

    Dependencies config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./src/**/*.{js, jsx, ts, tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        lobster: ['Lobster Two', 'cursive'],
+        raleway: ['Raleway', 'serif'],
+      },
+      gridTemplateColumns: {
+        productsLg: 'repeat(4, auto)',
+        productsSm: 'repeat(2, auto)',
+      },
+    },
+  },
+  plugins: [],
+};
