71122871e4589bf292fb140f79f17b005c39de37
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-21 09:32:57 +0630

    Add CartItem.js

---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,37 @@
+import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+const CartItem = ({
+  title,
+  image: imgSrc,
+  price,
+  quantity,
+  id,
+  increaseQuantiy,
+  decreaseQuantity,
+}) => {
+  return (
+    <div>
+      <img src={imgSrc} alt={title} />
+      <div>
+        <div>{title}</div>
+        <div>
+          <span>Qty: </span>
+          <span>
+            <button onClick={() => increaseQuantiy(id)}>
+              <FontAwesomeIcon icon={faCaretUp} />
+            </button>
+            <div>{quantity}</div>
+            <button onClick={() => decreaseQuantity(id)}>
+              <FontAwesomeIcon icon={faCaretDown} />
+            </button>
+          </span>
+        </div>
+        <div>{price}</div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
