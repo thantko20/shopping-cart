5d077468b87275cf057d62a328248d0c602663a6
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-21 15:09:44 +0630

    Add custom hook for calculating total amount

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,31 @@
 import { useState, useEffect } from 'react';
 
-const useTotalItems = (cart) => {
+const useCalculateTotalItems = (cart) => {
   const [totalItems, setTotalItems] = useState(0);
 
   useEffect(() => {
-    const newTotalAmount = cart.reduce((prevItem, currItem) => {
+    const newTotalItems = cart.reduce((prevItem, currItem) => {
       return prevItem.quantity + currItem.quantity;
     }, 0);
 
-    setTotalItems(newTotalAmount);
+    setTotalItems(newTotalItems);
   }, [cart]);
 
   return totalItems;
 };
+
+const useCalculateTotalAmount = (cart) => {
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  useEffect(() => {
+    const newTotalAmount = cart.reduce((prevItem, currItem) => {
+      return (
+        prevItem.price * prevItem.quantity + currItem.price * currItem.quantity
+      );
+    }, 0);
+
+    setTotalAmount(newTotalAmount);
+  }, [cart]);
+};
+
+export { useCalculateTotalItems, useCalculateTotalAmount };
