a877ca8ee012a1e9e48fd5892e7790e1be73f462
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-24 11:22:34 +0630

    Use react-router built-in NavLink for active state

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -1,19 +1,30 @@
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import NavLink from './NavLink';
+// import NavLink from './NavLink';
+import { NavLink } from 'react-router-dom';
 
 const Nav = ({ totalItems }) => {
   return (
-    <ul className='flex items-center gap-5'>
+    <ul className='flex items-center gap-5 text-gray-800'>
       <li>
-        <NavLink path='/shop'>Shop</NavLink>
+        <NavLink
+          to='/shop'
+          className={({ isActive }) => (isActive ? 'font-semibold' : '')}
+        >
+          Shop
+        </NavLink>
       </li>
       <li>
-        <NavLink path='/about'>About</NavLink>
+        <NavLink
+          to='/about'
+          className={({ isActive }) => (isActive ? 'font-semibold' : '')}
+        >
+          About
+        </NavLink>
       </li>
       <li>
-        <NavLink path='/cart'>
+        <NavLink to='/cart'>
           <div className='relative'>
             <div className='absolute top-[-6px] right-[-6px] flex h-4 w-4 items-center justify-center rounded-full bg-rose-600 p-1 text-sm text-white'>
               {totalItems}
--- src/components/Header/NavLink.js
+++ src/components/Header/NavLink.js
@@ -1,8 +1,9 @@
+// Unused component
 import { Link } from 'react-router-dom';
 
 const NavLink = ({ path, children }) => {
   return (
-    <Link to={path} className='text-black text-base font-semibold'>
+    <Link to={path} className='text-base font-semibold text-black'>
       {children}
     </Link>
   );
